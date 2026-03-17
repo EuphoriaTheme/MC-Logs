92da471332eecf40596b8672fbfb689cf4ede04d
KingIronMan2011 <176780813+KingIronMan2011@users.noreply.github.com>
2026-03-17 21:41:58 +0000

    Refactor MclogsController structure and update route references

--- routes/client.php
+++ routes/client.php
@@ -1,10 +1,10 @@
 <?php
 
 Route::prefix('/api/client/servers')->middleware(['auth:sanctum'])->group(function () {
-    Route::get('/{uuid}/mclogs', 'Blueprint\Extensions\Mclogs\client\MclogsController@getUploads');
+    Route::get('/{uuid}/mclogs', 'Blueprint\Extensions\Mclogs\controllers\MclogsController@getUploads');
 
-    Route::post('/{uuid}/mclogs', 'Blueprint\Extensions\Mclogs\client\MclogsController@storeUpload');
+    Route::post('/{uuid}/mclogs', 'Blueprint\Extensions\Mclogs\controllers\MclogsController@storeUpload');
 
-    Route::delete('/{uuid}/mclogs/{mclogs_id}', 'Blueprint\Extensions\Mclogs\client\MclogsController@deleteUpload');
-    Route::delete('/{uuid}/mclogs', 'Blueprint\Extensions\Mclogs\client\MclogsController@clearAllUploads');
+    Route::delete('/{uuid}/mclogs/{mclogs_id}', 'Blueprint\Extensions\Mclogs\controllers\MclogsController@deleteUpload');
+    Route::delete('/{uuid}/mclogs', 'Blueprint\Extensions\Mclogs\controllers\MclogsController@clearAllUploads');
 });
